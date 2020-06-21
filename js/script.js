701b2c3a7556ba799dc81e4bb84427df664428f8
Kashifrazabb <Kashifrazabb@gmail.com>
2020-06-21 13:37:00 -0700

    animation

--- js/script.js
+++ js/script.js
@@ -34,14 +34,28 @@ $(document).ready(function () {
     $('.pics .row').css('display', 'block');
     $('#' + e.target.id).css({ color: 'white', background: 'red' });
     $('.portfolio-section a').not('#' + e.target.id).css({ color: 'black', background: 'transparent' });
-    if (e.target.id == 'app') { $('.pics .row').not('.' + 'app').css('display', 'none') }
-    if (e.target.id == 'web') { $('.pics .row').not('.' + 'web').css('display', 'none') }
-    if (e.target.id == 'card') { $('.pics .row').not('.' + 'card').css('display', 'none') }
+    if (e.target.id == 'app') { $('.pics .row').not('.' + 'APP').css('display', 'none') }
+    if (e.target.id == 'web') { $('.pics .row').not('.' + 'WEB').css('display', 'none') }
+    if (e.target.id == 'card') { $('.pics .row').not('.' + 'CARD').css('display', 'none') }
     if (e.target.id == 'all') { $('.pics .row').css('display', 'block') }
     })
 
-    $('.navbar .dropdown > a').click(()=>{
-        $('.navbar .dropdown > a').toggleClass('special')
+    $('.navbar .dropdown > a').click(()=>
+        $('.navbar .dropdown > a').toggleClass('special'))
+
+    $('.pics .row').mouseenter((e)=>{
+        var target=e.target.className.split(' ')[1]
+        $(e.target).append('<div class="content">\
+        <h5>'+ target+ ' '+ e.target.id+'<h5>\<h6>'+target+'<h6/>\</div>')
     })
+
+    $('.pics .row').mouseleave((e)=>$('.pics .row .content').remove())
+
+    $('.carousel-caption h1').addClass('animate__animated animate__slideInDown').
+    css('animation-duration','1s')
+    $('.carousel-caption p').addClass('animate__animated animate__slideInLeft').
+    css('animation-duration','2s')
+    $('.carousel-caption button').addClass('animate__animated animate__slideInUp').
+    css('animation-duration','3s')
 
 });
