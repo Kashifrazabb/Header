bf29a84be9fa2a1d495d1d26b96b72b198b222f1
Kashifrazabb <Kashifrazabb@gmail.com>
2020-06-22 04:04:23 -0700

    slishow :(

--- js/script.js
+++ js/script.js
@@ -58,4 +58,32 @@ $(document).ready(function () {
     $('.carousel-caption button').addClass('animate__animated animate__slideInUp').
     css('animation-duration','1s')
 
+    $('#slideshow').on('slide.bs.carousel', function (e) {
+
+        /*
+    
+        CC 2.0 License Iatek LLC 2018
+        Attribution required
+        
+        */
+    
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 3;
+        var totalItems = $('.carousel-item').length;
+        
+        if (idx >= totalItems-(itemsPerSlide-1)) {
+            var it = itemsPerSlide - (totalItems - idx);
+            for (var i=0; i<it; i++) {
+                // append slides to end
+                if (e.direction=="left") {
+                    $('#slideshow .carousel-item').eq(i).appendTo('.carousel-inner');
+                }
+                else {
+                    $('#slideshow .carousel-item').eq(0).appendTo('.carousel-inner');
+                }
+            }
+        }
+    });
+
 });
