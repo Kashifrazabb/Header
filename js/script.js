128c5a549de7b1ec81ad404b87e15c02e9816ee4
Kashifrazabb <Kashifrazabb@gmail.com>
2020-06-24 01:06:43 -0700

    sss

--- js/script.js
+++ js/script.js
@@ -62,4 +62,8 @@ $(document).ready(function () {
     document.ontouchmove = function(e){
         e.preventDefault();
         }
+        input.onfocus = function () {
+            window.scrollTo(0, 0);
+            document.body.scrollTop = 0;
+        }
 });
